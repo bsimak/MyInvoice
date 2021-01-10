db0f2d530d1ea2cee2f8403faecabbdb572e5dda
B.simak <b.simak@gmail.com>
2021-01-10 23:15:31 +0100

    First Login Screen

--- src/main/java/org/example/web/WebsiteController.java
+++ src/main/java/org/example/web/WebsiteController.java
@@ -1,9 +1,12 @@
 package org.example.web;
 
+import org.example.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
 
@@ -15,5 +18,21 @@ public class WebsiteController {
         model.addAttribute("username", username);
         model.addAttribute("currentDate", new Date());
         return "index.html";
+    }
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("currentDate", new Date());
+        model.addAttribute("loginForm", new LoginForm());
+        return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+        if (loginForm.getUsername().equals(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("currentDate", new Date());
+        model.addAttribute("invalidCredentials", "true");
+        return "login.html";
     }
 }
---
+++ src/main/java/org/example/web/forms/LoginForm.java
@@ -0,0 +1,19 @@
+package org.example.web.forms;
+
+public class LoginForm {
+    private String username ="Barbara";
+    private String password = "";
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername (String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword (String password) {
+        this.password = password;
+    }
+}
